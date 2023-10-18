2f0350d8df41bd13ff7fd27e4d610085db02c2d3
ThanhDeskTop <thanhdd19988@gmail.comm>
2023-10-18 23:44:20 +0700

    #7 Success Response Class

---
+++ src/core/success.response.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const StatusCode = {
+    OK: 200,
+    CREATE: 201,
+};
+
+const ReasonStatusCode = {
+    OK: 'Success',
+    CREATE: 'Created!',
+};
+
+class SuccessResponse {
+    constructor({ message, statusCode = StatusCode.OK, reasonStatusCode = ReasonStatusCode.OK, metadata = {} }) {
+        this.message = message ? message : reasonStatusCode;
+        this.status = statusCode;
+        this.metadata = metadata;
+    }
+
+    send(res, headers = {}) {
+        return res.status(this.status).json(this);
+    }
+}
+
+class OK extends SuccessResponse {
+    constructor({ message, metadata }) {
+        super(message, metadata);
+    }
+}
+
+class CREATED extends SuccessResponse {
+    constructor({
+        message,
+        statusCode = StatusCode.CREATE,
+        reasonStatusCode = ReasonStatusCode.CREATE,
+        metadata = {},
+        option = {},
+    }) {
+        super({ message, statusCode, reasonStatusCode, metadata });
+        this.option = option;
+    }
+}
+
+module.exports = {
+    OK,
+    CREATED,
+};
