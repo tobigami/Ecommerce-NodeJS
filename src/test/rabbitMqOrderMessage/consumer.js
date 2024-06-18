c3e97f53a30832eac10449dc651bce1cb1e0fa2b
SBC-ThanhDD <thanhdd@bsscommerce.com>
2024-06-18 23:50:02 +0700

    #21.5 Order message

---
+++ src/test/rabbitMqOrderMessage/consumer.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const amqplib = require('amqplib');
+
+const consumer = async () => {
+    try {
+        const connection = await amqplib.connect('amqp://guest:12345@localhost');
+
+        const channel = await connection.createChannel();
+        const queueName = 'order-message';
+
+        await channel.assertQueue(queueName, {
+            durable: true
+        });
+
+        // set prefetch to 1 ensure only one ack at a time
+        channel.prefetch(1);
+
+        channel.consume(queueName, (msg) => {
+            const message = msg.content.toString();
+            setTimeout(() => {
+                console.log('processed', message);
+                channel.ack(msg);
+            }, Math.random() * 1000);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+consumer().catch((error) => console.log(error));
---
+++ src/test/rabbitMqOrderMessage/producer.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const amqplib = require('amqplib');
+
+const producer = async () => {
+    try {
+        const connection = await amqplib.connect('amqp://guest:12345@localhost');
+
+        const channel = await connection.createChannel();
+
+        const queueName = 'order-message';
+
+        await channel.assertQueue(queueName, {
+            durable: true
+        });
+
+        for (let i = 0; i < 10; i++) {
+            const msg = `message ${i}`;
+            channel.sendToQueue(queueName, Buffer.from(msg), {
+                persistent: true
+            });
+        }
+
+        setTimeout(() => {
+            connection.close();
+            process.exit(0);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+producer().catch((error) => console.log(error));
