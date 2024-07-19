d7c842ca3162004d72b8401e971e2cfb8d539d36
SBC-ThanhDD <thanhdd@bsscommerce.com>
2024-07-19 17:43:45 +0700

    #23 insert 1000000 recored mysql

--- src/test/mysql/connect.mysql.test.js
+++ src/test/mysql/connect.mysql.test.js
@@ -1,7 +1,4 @@
-require('dotenv').config()
-
 const mysql = require('mysql2')
-
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'test',
@@ -9,14 +6,50 @@ const pool = mysql.createPool({
     database: 'shopDev'
 })
 
-pool.query('SELECT * from users', function(err, result) {
-    
-    if(err) throw err
+const batchSize = 100000;
+const totalSize = 1_000_000;
 
-    console.log('result', result)
+let currentId = 1
+console.time()
+const insertBatch = async () => {
+    const value = []
+    for (let i = 0; i < batchSize && currentId <= totalSize; i++) {
+        const name = `Name: ${currentId}`;
+        const age = currentId;
+        const address = `address-${currentId}`
+        value.push([currentId, name, age, address])
+        currentId++        
+    }
 
-    pool.end(err => {
+    if(!value.length) {
+        pool.end((err) => {
+            console.timeEnd()
+            if(err) {
+                console.log('close pool error')
+            } else {
+                console.log('close pool error successfully')
+            }
+        })
+        return
+    }
+    const sql = `INSERT INTO test_table (id, name, age, address) VALUES ?`
+    pool.query(sql, [value], async function(err, result) {
         if(err) throw err
-        console.log('close pool connection')
+        console.log(`inserted ${result.affectedRows} record`)
+        await insertBatch()
     })
-})
+}
+
+insertBatch().catch(err => console.log(err))
+
+// pool.query('SELECT * from users', function(err, result) {
+    
+//     if(err) throw err
+
+//     console.log('result', result)
+
+//     pool.end(err => {
+//         if(err) throw err
+//         console.log('close pool connection')
+//     })
+// })
